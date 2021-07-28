fe0775b7125e70effacbd4b4ec5a25fc383c5a30
joasmoura <40508788+joasmoura@users.noreply.github.com>
2021-07-28 19:47:51 -0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,8 +49,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: "http://127.0.0.1:8000/api/"
-    // baseURL: "https://webserver.rapidopedido.com.br/api/"
+    // baseURL: "http://127.0.0.1:8000/api/"
+    baseURL: "https://webserver.rapidopedido.com.br/api/"
   },
 
   auth: {
